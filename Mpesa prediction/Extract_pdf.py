c1ba873ad8fd34b402ceac50fe8ca5eab54e86af
Brian kimanzi <migelbrian3@gmail.com>
2025-11-03 13:27:20 +0300

    new code update

--- Mpesa prediction/Extract_pdf.py
+++ Mpesa prediction/Extract_pdf.py
@@ -4,11 +4,5 @@ import PyPDF2
 import pikepdf
 import warnings
 warnings.filterwarnings('ignore')
-import matplotlib.pyplot as plt
-import seaborn as sns
-import plotly.express as px
-import plotly.graph_objects as go
 from tabula.io import read_pdf
 from dateutil import parser
-from sqlalchemy import create_engine
-import mysql.connector
