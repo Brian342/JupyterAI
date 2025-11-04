bcac461c492f789ea313b95ef03d4484f2b59a3f
Brian kimanzi <migelbrian3@gmail.com>
2025-11-04 08:59:23 +0300

    new code update

--- Mpesa prediction/Extract_pdf.py
+++ Mpesa prediction/Extract_pdf.py
@@ -1,28 +1,21 @@
 import io
 import tempfile
-
 import pandas as pd
 import numpy as np
 import PyPDF2
 import pdfplumber
 import pikepdf
 import warnings
-
 warnings.filterwarnings('ignore')
 from tabula.io import read_pdf
 from dateutil import parser
 
-input_path = "/Users/briankimanzi/Downloads/Mpesa pdfs/Statement_All_Transactions_20240901_20250301.pdf"
-output_path = "/Users/briankimanzi/Downloads/Mpesa pdfs/Tracking.pdf"
-password = "102030"
 
-with pikepdf.open(input_path, password=password) as pdf:
-    pdf.save(output_path)
-path = output_path
-
-
-def extract_mpesa_data(uploaded_file, password=None):
+def extract_mpesa_data(uploaded_file, password=""):
     try:
+        if password is None:
+            password = ""
+
         file_bytes = uploaded_file.read()
 
         decrypted_bytes = io.BytesIO()
