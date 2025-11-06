eb0afc17f4f356482b32fefcb8295de68900b39e
Brian kimanzi <migelbrian3@gmail.com>
2025-11-07 00:36:24 +0300

    new code update

--- Mpesa prediction/model.py
+++ Mpesa prediction/model.py
@@ -1,2 +1,10 @@
 import pandas as pd
 df_model = pd.read_csv("statement_cleaned")
+df_model['Date'] = pd.to_datetime(df_model['Date'], errors='coerce')
+
+# keeping only outgoing (spending) transactions
+df_model = df_model[df_model['paid_in_or_Withdraw'].str.contains('Withdraw', case=False, na=False)]
+
+# handling missing values
+df_model = df_model.dropna(subset=['Transaction_amount', 'Date'])
+df_model = df_model[df_model['Transaction_amount'] >0 ]
