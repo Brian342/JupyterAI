f982e347dcbed20879158b5d7ac912f63cfe7852
Brian kimanzi <migelbrian3@gmail.com>
2025-11-07 00:49:25 +0300

    new code update

--- Mpesa prediction/model.py
+++ Mpesa prediction/model.py
@@ -11,3 +11,27 @@ df_model = df_model.dropna(subset=['Transaction_amount', 'Date'])
 df_model = df_model[df_model['Transaction_amount'] > 0]
 
 df_model['is_weekend'] = df_model['Weekday'].apply(lambda x: 1 if x >= 5 else 0)
+df_model = df_model.sort_values('Date')
+df_model['rolling_avg_spent'] = df_model['Transaction_amount'].rolling(window=7, min_periods=1).mean()
+
+
+# transaction time category
+def time_group(h):
+    if 5 <= h <= 12:
+        return 'Morning'
+    elif 12 <= h <= 18:
+        return 'Afternoon'
+    else:
+        return 'Night'
+
+
+df_model['time_period'] = df_model['Hour'].apply(time_group)
+
+# Agg Daily Spending's
+daily_df = (
+    df_model.groupby('Date')['Transaction_amount']
+    .sum()
+    .reset_index()
+    .rename(columns={'Transaction_amount': 'Daily_Spend'})
+)
+
