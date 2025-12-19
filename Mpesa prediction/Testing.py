d02888f4d6e2070b90bfb3b49a6ec9ddf9939bcf
Brian kimanzi <migelbrian3@gmail.com>
2025-12-19 20:57:55 +0300

    new code update

--- Mpesa prediction/Testing.py
+++
@@ -1,9 +0,0 @@
-import os
-file_path = 'Models/Mpesa_LinearRegression.pkl'
-try:
-    with open(file_path, 'rb') as f:
-        print(f"[✔️]Managed to open file: {os.path.basename(file_path)}")
-except FileNotFoundError:
-    print(f"[𝒳]Could not open the file {file_path}")
-except Exception as e:
-    print(f"[𝑥]An error occurred when opening the file {e}")
